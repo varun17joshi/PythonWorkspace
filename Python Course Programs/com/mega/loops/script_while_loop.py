bb3d1c3cbf10cb62391ea9d83bbfa99fb0b9afcd
Varun Joshi <vjoshi@cayan.com>
2017-12-10 14:03:40 +0530

    Added program for while loop

---
+++ Python Course Programs/com/mega/loops/script_while_loop.py
@@ -0,0 +1,12 @@
+'''
+Created on Dec 10, 2017
+
+@author: VarunJoshi
+'''
+password = ''
+while password != "varun173":
+    password = input("enter the password")
+    if password == "varun173":
+        print("Password is correct")
+    else:
+        print("Sorry !! Try Again!!")
