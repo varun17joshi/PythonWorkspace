dbcd000c37bb9da8ab20eb5078154ab2bed8c7e9
Varun Joshi <vjoshi@cayan.com>
2017-12-10 14:27:00 +0530

    Added programs for loop with lists

---
+++ Python Programs/com/mega/loops/loops_with_list.py
@@ -0,0 +1,11 @@
+'''
+Created on Dec 10, 2017
+
+@author: VarunJoshi
+'''
+names = ['varun','akshay','chandrashekhar']
+domains = ['@gmail.com','@yahoo.com','@hotmail.com']
+
+for name in names:
+    for domain in domains:
+        print(name+domain)
